586eb2ba5bbfed71db247d9b4e3508a15bd396b3
Oksana911 <rosa111@mail.ru>
2022-10-20 14:44:20 +0700

    password-hash

--- users/serializers.py
+++ users/serializers.py
@@ -40,6 +40,7 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
+        user.set_password(user.password)
 
         for loc in self._location:
             loc_obj, _ = Location.objects.get_or_create(name=loc)
